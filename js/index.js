311aa48c6813cf0b59be80571976c8fb64df11ae
himalaya-31 <linkinparkhoney@gmail.com>
2018-09-24 15:13:20 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,33 +1,19 @@
 $(document).ready(function () {
-    /*$(window).scroll(function () {
+    $(window).scroll(function () {
         var body = $(this).scrollTop();
         var windowWidth = $(window).width();
 
         if (windowWidth <= 600) {
-            var personalTop = $("#personal").offset().top;
+            var personalTop = $("#startup").offset().top + $("#startup").outerHeight(true);
 
             if (body > personalTop) {
                 console.log("if");
-                $(".social-accounts").animate({right: '-80px'});
+                $(".social-accounts").stop().animate({right: '-80px'});
             }
             else {
                 console.log("else");
-                $(".social-accounts").animate({right: '0px'});
+                $(".social-accounts").stop().animate({right: '0px'});
             }
         }
-    });*/
+    });
 });
-
-
-/*if (body > personalTop) {
-    if ($(".social-accounts").hasClass("social-acc-anim-2")) {
-        $(".social-accounts").removeClass("social-acc-anim-2");
-    }
-    $(".social-accounts").addClass("social-acc-anim-1");
-}
-else {
-  if ($(".social-accounts").hasClass("social-acc-anim-1")) {
-      $(".social-accounts").removeClass("social-acc-anim-1");
-  }
-  $(".social-accounts").addClass("social-acc-anim-2");
-}*/
